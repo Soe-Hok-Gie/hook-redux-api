479d35f4259a9e00c2d5e779d89816e625f69469
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-25 01:05:47 +0700

    membuat store kemudian mengimport reducer yg telah dibuat tadi. juga ada compose dan applyMiddleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import {createStore, compose, appliMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 
+import reducers from './reducer'
+
+const store = createStore (reducers, compose(appliMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
