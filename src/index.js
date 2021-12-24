fae3a7104e41371f552c1cf220b887426497ede2
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-25 00:55:41 +0700

    import yang dibutuhkan untuk react redux, redux dan redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {createStore, compose, appliMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
