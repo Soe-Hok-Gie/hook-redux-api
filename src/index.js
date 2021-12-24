66a8674239050235f5119678c8e7551af87d502e
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-25 01:07:15 +0700

    component App kita bungkus dgn provider

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const store = createStore (reducers, compose(appliMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+  
