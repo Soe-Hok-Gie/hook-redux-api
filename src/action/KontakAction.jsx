1f770afbd4d5d526b91c362a7e54bfd38808cb7a
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-26 02:34:59 +0700

    pertama membuat distpatc untuk loading

--- src/action/KontakAction.jsx
+++ src/action/KontakAction.jsx
@@ -5,4 +5,14 @@ export const GET_LIST_KONTAK = "GET_LIST_KONTAK";
 export const getListKontak = () =>{
     return(dispatch) =>{
 
-    }}
+            //loading
+            dispatch({
+            type:GET_LIST_KONTAK,
+            payload:{
+                loading:true,
+                data:false,
+                errorMessage:false
+            }
+
+    })
+}}
