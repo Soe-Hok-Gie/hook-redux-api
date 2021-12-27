75a6858795500075934c30bdba50fc0a4b925b7e
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-28 00:09:45 +0700

    menampilkan data Api dgn Mapping (karena dalam bentuk array. biasakan berikan 4 opsi : a. berhasil, b. loading, c. error d. jika ketiga-tiganya salah )

--- src/component/listKontak/IndexKontak.jsx
+++ src/component/listKontak/IndexKontak.jsx
@@ -1,7 +1,7 @@
-import React , { useEffect }from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { getListKontak } from '../../action/KontakAction';
-import {useDispatch,useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 const Container = styled.div``;
@@ -10,24 +10,33 @@ const Title = styled.h4``;
 
 const IndexKontak = () => {
 
-    const {getListKontakResult,getListKontakLoading,getListKontakError} =  useSelector(state => state.KontakReducer)
+    const { getListKontakResult, getListKontakLoading, getListKontakError } = useSelector(state => state.KontakReducer)
     const dispatch = useDispatch()
 
 
     useEffect(() => {
 
-    //panggil action getListcontak
-    console.log("1. useEffect component DId Mount")
-    dispatch(getListKontak()); 
+        //panggil action getListcontak
+        console.log("1. useEffect component DId Mount")
+        dispatch(getListKontak());
 
 
-     },[dispatch])
+    }, [dispatch])
 
     return (
         <Container>
-            <Title>
-                list kontak
-            </Title>
+            <Title>list kontak </Title>
+            {getListKontakResult ? (
+                getListKontakResult.map((kontak) => {
+                    return (
+                        <p key={kontak.id}> {kontak.nama}-{kontak.nohp}</p>
+                    )
+                })
+            ) : getListKontakLoading ? (
+                <p>Loading ...</p>
+            ) : (
+                <p> {getListKontakError ? getListKontakError : "data kosong "}</p>
+            )}
         </Container>
     )
 }
