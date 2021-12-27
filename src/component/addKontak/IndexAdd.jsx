7bccba633b86addb3960c7547d5a8170438d00af
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-28 00:52:51 +0700

    membuat form inputan

--- src/component/addKontak/IndexAdd.jsx
+++ src/component/addKontak/IndexAdd.jsx
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 
 const Container = styled.div``;
 const Title = styled.h4``;
+const Form = styled.form``;
+const Input = styled.input``;
+
 
 
 const IndexAdd = () => {
     return (
         <Container>
             <Title>Add Kontak</Title>
+            <Form>
+                <Input type="text" name="nama" placeholder="Nama"/>
+                <Input type="number" name="nohp" placeholder="No Hp"/>
+            </Form>
         </Container>
     )
 }
