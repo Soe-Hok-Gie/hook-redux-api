383aa919b4ad7455e471f12bfaf5fac0dafb7e40
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-29 22:58:36 +0700

    cukup buat satu case detail_kontak

--- src/reducer/kontak/Kontak.jsx
+++ src/reducer/kontak/Kontak.jsx
@@ -1,48 +1,54 @@
-import {GET_LIST_KONTAK,ADD_KONTAK,DELETE_KONTAK,DETAIL_KONTAK} from '../../action/KontakAction'
+import { GET_LIST_KONTAK, ADD_KONTAK, DELETE_KONTAK, DETAIL_KONTAK } from '../../action/KontakAction'
 
 
-const initialState ={
-    getListKontakResult : false,
-    getListKontakLoading : false,
-    getListKontakError : false,
+const initialState = {
+    getListKontakResult: false,
+    getListKontakLoading: false,
+    getListKontakError: false,
 
-    addKontakResult : false,
-    addKontakLoading : false,
-    addKontakError : false,
+    addKontakResult: false,
+    addKontakLoading: false,
+    addKontakError: false,
 
-    deleteKontakResult : false,
-    deleteKontakLoading : false,
-    deleteKontakError : false,
+    deleteKontakResult: false,
+    deleteKontakLoading: false,
+    deleteKontakError: false,
 
-    detailKontakResult : false
+    detailKontakResult: false
 }
 
-const kontak = ( state =initialState, action) =>{
+const kontak = (state = initialState, action) => {
     switch (action.type) {
         case GET_LIST_KONTAK:
             // console.log("4. masuk reduce :",action)
-            return{
+            return {
                 ...state,
                 getListKontakResult: action.payload.data,
                 getListKontakLoading: action.payload.loading,
-                getListKontakError : action.payload.errormessage
+                getListKontakError: action.payload.errormessage
             }
-            case ADD_KONTAK:
-                // console.log("4. masuk reduce :", action)
-                return{
-                    ...state,
-                    addKontakResult: action.payload.data,
-                    addKontakLoading: action.payload.loading,
-                    addKontakError : action.payload.errormessage
-                }
-                case DELETE_KONTAK:
-                    console.log("4. masuk reduce :", action)
-                    return{
-                        ...state,
-                        deleteKontakResult: action.payload.data,
-                        deleteKontakLoading: action.payload.loading,
-                        deleteKontakError : action.payload.errormessage
-                    }
+        case ADD_KONTAK:
+            // console.log("4. masuk reduce :", action)
+            return {
+                ...state,
+                addKontakResult: action.payload.data,
+                addKontakLoading: action.payload.loading,
+                addKontakError: action.payload.errormessage
+            }
+        case DELETE_KONTAK:
+            // console.log("4. masuk reduce :", action)
+            return {
+                ...state,
+                deleteKontakResult: action.payload.data,
+                deleteKontakLoading: action.payload.loading,
+                deleteKontakError: action.payload.errormessage
+            }
+        case DETAIL_KONTAK:
+            console.log("4. masuk reduce :", action)
+            return {
+                ...state,
+                detailKontakResult: action.payload.data
+            }
         default:
             return state;
     }
