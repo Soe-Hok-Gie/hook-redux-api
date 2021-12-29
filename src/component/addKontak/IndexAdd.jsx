eb15ec490074b4dc4484371559540d76c581bd7c
Soe-Hok-Gie <wordcoffe0@gmail.com>
2021-12-30 01:21:26 +0700

    menambahkan hook effect supaya data edit langsung berganti tanpa harus tereload

--- src/component/addKontak/IndexAdd.jsx
+++ src/component/addKontak/IndexAdd.jsx
@@ -1,7 +1,7 @@
-import React , { useEffect, useState }from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { useDispatch, useSelector} from 'react-redux'
-import { addKontak,getListKontak, updateKontak } from '../../action/KontakAction';
+import { useDispatch, useSelector } from 'react-redux'
+import { addKontak, getListKontak, updateKontak } from '../../action/KontakAction';
 
 
 
@@ -13,74 +13,83 @@ const Button = styled.button``;
 
 
 const IndexAdd = () => {
-    const [nama, setNama] = useState("");
-    const [nohp, setNohp] = useState("");
-    const [id, setId] = useState("");
+  const [nama, setNama] = useState("");
+  const [nohp, setNohp] = useState("");
+  const [id, setId] = useState("");
 
- 
-    const {addKontakResult,detailKontakResult} = useSelector((state) => state.KontakReducer)
-    const dispatch = useDispatch();
 
-    // jika ada logika atau keperluan yg panjang pakek cara pertama
-    // const onChangeNama = (e) => {
-    //     const nama= e.target.value;
-    //     setNama(nama);
-    // };
-    // const onChangeNohp = (e) => {
-    //     const nohp= e.target.value;
-    //     setNohp(nohp);
-    // };
+  const { addKontakResult, detailKontakResult,updateKontakResult } = useSelector((state) => state.KontakReducer)
+  const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log ("1. masuk hendlesubmit")
-        if (id) {
-        //update kontak
-        dispatch (updateKontak({id: id, nama: nama, nohp : nohp}))
-    
-        } else {
-            //add kontak
-            dispatch (addKontak({nama: nama, nohp : nohp}))
-        }
-     }
+  // jika ada logika atau keperluan yg panjang pakek cara pertama
+  // const onChangeNama = (e) => {
+  //     const nama= e.target.value;
+  //     setNama(nama);
+  // };
+  // const onChangeNohp = (e) => {
+  //     const nohp= e.target.value;
+  //     setNohp(nohp);
+  // };
 
-    useEffect(() => {
-      if (addKontakResult) {
-        // console.log ('5. masuk component Did update')
-        dispatch (getListKontak ()); 
-        setNama ('');
-        setNohp ('');
-      }
-    }, [addKontakResult,dispatch])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("1. masuk hendlesubmit")
+    if (id) {
+      //update kontak
+      dispatch(updateKontak({ id: id, nama: nama, nohp: nohp }))
 
-    useEffect(() => {
-        if (detailKontakResult) {
-          console.log ('5. masuk component Did update')
-          dispatch (getListKontak ()); 
-          setNama (detailKontakResult.nama);
-          setNohp (detailKontakResult.nohp);
-          setId (detailKontakResult.id)
-        }
-      }, [detailKontakResult,dispatch])
+    } else {
+      //add kontak
+      dispatch(addKontak({ nama: nama, nohp: nohp }))
+    }
+  }
 
-    return (
-        <Container>
-            {/* <Title>Add Kontak</Title>
+  useEffect(() => {
+    if (addKontakResult) {
+      // console.log ('5. masuk component Did update')
+      dispatch(getListKontak());
+      setNama('');
+      setNohp('');
+    }
+  }, [addKontakResult, dispatch])
+
+  useEffect(() => {
+    if (detailKontakResult) {
+      //   console.log ('5. masuk component Did update')
+      dispatch(getListKontak());
+      setNama(detailKontakResult.nama);
+      setNohp(detailKontakResult.nohp);
+      setId(detailKontakResult.id);
+    }
+  }, [detailKontakResult, dispatch])
+
+  useEffect(() => {
+    if (updateKontakResult) {
+      console.log ('5. masuk component Did update',updateKontakResult)
+      dispatch(getListKontak());
+      setNama('');
+      setNohp('');
+    }
+  }, [updateKontakResult, dispatch])
+
+  return (
+    <Container>
+      {/* <Title>Add Kontak</Title>
             <Form onSubmit={handleSubmit}>
                 <Input type="text" name="nama" placeholder="Nama"  value={nama} onChange={onChangeNama}/>
                 <Input type="number" name="nohp" placeholder="No Hp" value={nohp} onChange={onChangeNohp}/>
                 <Button type="submit">Submit</Button>
             </Form> */}
 
-             <Title>Add Kontak</Title>
-              {/* //cara ke 2 apabila tidak ada logika yg panjang */}
-            <Form onSubmit={(e) => handleSubmit (e)}>
-                <Input type="text" name="nama" placeholder="Nama" value={nama} onChange={(e) =>setNama(e.target.value)}/>
-                <Input type="number" name="nohp" placeholder="No Hp" value={nohp} onChange={(e) =>setNohp(e.target.value)}/>
-                <Button type="submit">Submit</Button>
-            </Form>
-        </Container>
-    )
+      <Title>Add Kontak</Title>
+      {/* //cara ke 2 apabila tidak ada logika yg panjang */}
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Input type="text" name="nama" placeholder="Nama" value={nama} onChange={(e) => setNama(e.target.value)} />
+        <Input type="number" name="nohp" placeholder="No Hp" value={nohp} onChange={(e) => setNohp(e.target.value)} />
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
+  )
 }
 
 export default IndexAdd
